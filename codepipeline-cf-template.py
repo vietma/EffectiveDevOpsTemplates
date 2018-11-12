348e11b987b14bbf8c57fb001dfafff870d27e82
p752221 <viet.c.ma@nab.com.au>
2018-11-12 15:01:57 +1100

    add codepipeline cf template

---
+++ codepipeline-cf-template.py
@@ -0,0 +1,145 @@
+from awacs.aws import (
+    Allow,
+    Policy,
+    Principal,
+    Statement
+)
+
+from awacs.sts import AssumeRole
+
+from troposphere import (
+    Ref,
+    GetAtt,
+    Template
+)
+
+from troposphere.codepipeline import (
+    Actions,
+    ActionTypeId,
+    ArtifactStore,
+    InputArtifacts,
+    OutputArtifacts,
+    Pipeline,
+    Stages
+)
+
+from troposphere.iam import Role
+from troposphere.iam import Policy as IAMPolicy
+
+from troposphere.s3 import Bucket, VersioningConfiguration
+
+t = Template()
+
+t.add_description("Effective DevOps in AWS: Pipeline")
+
+t.add_resource(Bucket(
+    "S3Bucket",
+    VersioningConfiguration=VersioningConfiguration(
+        Status="Enabled"
+    )
+))
+
+t.add_resource(Role(
+    "PipelineRole",
+    AssumeRolePolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[AssumeRole],
+                Principal=Principal("Service", ["codepipeline.amazonaws.com"])
+            )
+        ]
+    ),
+    Path="/",
+    Policies=[
+        IAMPolicy(
+            PolicyName="HelloworldCodePipeline",
+            PolicyDocument={
+                "Statement": [
+                    {"Effect": "Allow", "Action": "cloudformation:*", "Resource": "*"},
+                    {"Effect": "Allow", "Action": "codepipeline:*", "Resource": "*"},
+                    {"Effect": "Allow", "Action": "s3:*", "Resource": "*"}
+                ]
+            }
+        )
+    ]
+))
+
+t.add_resource(Pipeline(
+    "HelloWorldPipeline",
+    RoleArn=GetAtt("PipelineRole", "Arn"),
+    ArtifactStore=ArtifactStore(
+        Type="S3",
+        Location=Ref("S3Bucket")
+    ),
+    Stages=[
+        Stages(
+            Name="Source",
+            Actions=[
+                Actions(
+                    Name="Source",
+                    ActionTypeId=ActionTypeId(
+                        Category="Source",
+                        Owner="ThirdParty",
+                        Version="1",
+                        Provider="GitHub"
+                    ),
+                    Configuration={
+                        "Owner": "ToBeConfiguredLater",
+                        "Repo": "ToBeConfiguredLater",
+                        "Branch": "ToBeConfiguredLater",
+                        "OAuthToken": "ToBeConfiguredLater"
+                    },
+                    OutputArtifacts=[
+                        OutputArtifacts(
+                            Name="helloworldApp"
+                        )
+                    ]
+                )
+            ]
+        ),
+        Stages(
+            Name="Staging",
+            Actions=[
+                Actions(
+                    Name="Deploy",
+                    ActionTypeId=ActionTypeId(
+                        Category="Deploy",
+                        Owner="AWS",
+                        Version="1",
+                        Provider="CloudFormation"
+                    ),
+                    Configuration={
+                        "ActionMode": "CREATE_UPDATE",
+                        "StackName": "helloworld-staging-service",
+                        "Capabilities": "CAPABILITY_NAMED_IAM",
+                        "TemplatePath": "helloworldApp::templates/nodeserver-cf.template"
+                    },
+                    InputArtifacts=[
+                        InputArtifacts(
+                            Name="helloworldApp"
+                        )
+                    ]
+                )
+            ]
+        ),
+        Stages(
+            Name="Approval",
+            Actions=[
+                Actions(
+                    Name="Approval",
+                    ActionTypeId=ActionTypeId(
+                        Category="Approval",
+                        Owner="AWS",
+                        Version="1",
+                        Provider="Manual"
+                    ),
+                    Configuration={},
+                    InputArtifacts=[]
+                )
+            ]
+        )
+    ]
+))
+
+print(t.to_json())
